e943d16d1232083f7176ccb508c5ca497c75b67c
mamulaidze <mamulaidzeg186@gmail.com>
2025-02-28 13:21:20 +0400

    hw

--- New/day 145/homeWork/src/App.jsx
+++ New/day 145/homeWork/src/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react';
 
 export default function App() {
+  const userName = useRef();
+  const [names, setNames] = useState(() => {
+    const savedNames = localStorage.getItem('names');
+    return savedNames ? JSON.parse(savedNames) : [];
+  });
+
+  const handleClick = () => {
+    const name = userName.current.value.trim()
+    if (name) {
+      const updatedNames = [...names, name]
+      setNames(updatedNames);
+      localStorage.setItem('names', JSON.stringify(updatedNames))
+      userName.current.value = '';
+    }
+  }
+  const handleDelete =() => {
+    localStorage.removeItem('names')
+  }
+
   return (
     <div>
-      
+      <h1 className="text-3xl">TO DO</h1>
+      <input ref={userName} placeholder="GUD BOY" />
+      <button onClick={handleClick}>დამატება</button>
+      <button className='pl-[30px]' onClick={handleDelete}>გასუფთავება</button>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
